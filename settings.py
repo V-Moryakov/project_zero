4f72447d8de80fa6d1daf80b48e40c6d7b65d3cd
Vladimir Moryakov <moryakov44@gmail.com>
2021-03-07 04:14:19 +0300

    finish screen background

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@
  	def __init__(self):
  		""" Инициализирует настройки игры."""
  		# Параметры экрана
- 		self.screen_width = 1200
- 		self.screen_height = 800
+ 		self.screen_width = 1920
+ 		self.screen_height = 1080
  		self.bg_color = (230, 230, 230)
